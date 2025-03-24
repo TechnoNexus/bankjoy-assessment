3a36ae6a53dd083f3aa787433eec6679594ea7e6
Archit Jain <t3chn0@Archits-MacBook-Pro.local>
2025-03-24 19:19:51 -0400

    Initial commit with API tests

---
+++ cypress/e2e/api/api_tests.js
@@ -0,0 +1,51 @@
+describe('API tests for Bank of Canada', () => {
+    const apiUrl = 'https://www.bankofcanada.ca/valet/observations/FXUSDCAD/json';
+    const recentWeeks = 10;
+  
+    it('Should fetch and average the forex rates for 10 weeks', () => {
+        cy.request(`${apiUrl}?recent_weeks=${recentWeeks}`)
+        .then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property('observations');
+          
+          const observations = response.body.observations;
+          expect(observations.length).to.be.greaterThan(0);
+  
+        let rates = [];
+        observations.forEach(param => {
+          let rate = parseFloat(param.FXUSDCAD.v);
+          rates.push(rate);
+        });
+
+        let total = 0;
+        rates.forEach(rate => {
+          total += rate;
+        });
+        let avgRate = total / rates.length;
+  
+          expect(avgRate).to.be.greaterThan(1.0);
+          expect(avgRate).to.be.lessThan(2.0);
+        });
+    });
+  
+    it('Should return 400 Bad Request for invalid date format', () => {
+      cy.request({
+        method: 'GET',
+        url: `${apiUrl}?recent_weeks=invalid`,
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(400);
+      });
+    });
+  
+    it('Should return 404 for invalid series code', () => {
+      cy.request({
+        method: 'GET',
+        url: 'https://www.bankofcanada.ca/valet/observations/INVALID_CODE/json?recent_weeks=10',
+        failOnStatusCode: false
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
+    });
+  });
+  
